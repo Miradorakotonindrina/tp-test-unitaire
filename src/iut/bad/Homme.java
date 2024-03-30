8a8538582220a09d9ba476c07bb63ca3667f0d6e
Mirado Rakotonindrina <rakotonindrinamirado@gmai.com>
2024-03-30 13:59:30 +0100

    deplacement des attribut dans la classe humain

--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,5 +1,12 @@
 package iut.bad;
 
-public class Homme {
-
+public class Homme extends Humain {
+	
+	public Homme(String nom, String prenom, String age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	 
 }
